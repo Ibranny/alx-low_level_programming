88aae7f58a806e5bf0e224620c60ee0de0db7fcd
Ibrahim <Ibranny69@gmail.com>
2022-09-08 12:30:57 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/*
- *   * main - Entry point
+#include <stdlib.h>
+
+
+
+/**
+ *  * main - Entry point
  *
- *    * Return: Always 1 (Success)
+ *   * Return: Always 1 (Success)
  *
  */
 
@@ -11,12 +15,10 @@ int main(void)
 
 {
 
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(STDOUT_FILENO, "and that piece of art is useful
 
+							\" - Dora Korpar, 2015-10-19\n", 59);
 
-
-			write(2, str1, 59);
-
-				return (1);
+			return (1);
 
 }

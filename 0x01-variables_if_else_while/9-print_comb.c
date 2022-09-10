fcc3725326c247d1b4ed3e104ee76caec8b17e27
Ibrahim <Ibranny69@gmail.com>
2022-09-09 17:24:53 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,25 +4,21 @@
  * main - Prints numbers between 0 to 9 and letters between a to f.
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
-
 {
-
 	int i;
 
 	for (i = 48; i < 58; i++)
-
 	{
-
 		putchar(i);
-
+	if (i != 57)
+	{
+		putchar(',');
+		putchar(' ');
 	}
-
-	putchar(',');
-	putchar(' ');
-
+	}
+	putchar('\n');
 	return (0);
 }
